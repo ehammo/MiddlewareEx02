48265703b3db7e0d5393002b7c8da6745da61b1d
ehammo <ehammo@cin.ufpe.br>
2018-08-25 20:29:50 -0300

    Added a '\n' to fix the communication
    Started benchmark

--- src/analise/benchmark.java
+++ src/analise/benchmark.java
@@ -2,6 +2,7 @@ package analise;
 
 import java.io.IOException;
 
+import tcp.ClientTcp;
 import tcp.ServerTcp;
 
 public class benchmark {
@@ -11,6 +12,11 @@ public class benchmark {
 		ServerTcp server = new ServerTcp();
 		try {
 			server.main(null);
+			int numberExecutions = 1000;
+			ClientTcp client = new ClientTcp("127.0.0.1", 2004);
+			for(int i = 0; i < numberExecutions; i++) {
+				
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
