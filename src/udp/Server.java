ecd3af72d179541458fb899648d08f9d6ba15d3f
ehammo <ehammo@cin.ufpe.br>
2018-08-25 19:50:04 -0300

    Port issue

--- src/udp/Server.java
+++ src/udp/Server.java
@@ -1,35 +1,28 @@
 package udp;
-import java.util.List;
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.HashMap;
+import udp.Estoque;
 
 public class Server {
     public static void main(String[] args) throws IOException {
          
-    	int portSend = 2005;
-		int portReceive = 2004;
+    	
+		int port = 2004;
         
         String in,out;
-        DatagramSocket  serverSocket = new DatagramSocket();
+        DatagramSocket  serverSocket = new DatagramSocket(port);
         byte[] receiveData = new byte[1024];
 		byte[] sendData = new byte[1024];
         Estoque estoque = new Estoque();
     	while (true){
     		DatagramPacket receivePacket = new DatagramPacket(receiveData,
-					receiveData.length, portReceive);
+					receiveData.length);
     		serverSocket.receive(receivePacket);
-    		InetAddress IPAddress = receivePacket.getAddress();
     		String[] command = new String(receivePacket.getData()).split(" ");
         	System.out.println("Data received.");
+        	
         	if (command[0].equals("add")) {
         		estoque.add(command[1]);
         		out = "Added with Sucess\n";
@@ -42,12 +35,13 @@ public class Server {
         		out = "Invalid command. Use 'add', 'remove' or 'list' \n";
         	}
         	sendData = out.getBytes();
+        	InetAddress IPAddress = receivePacket.getAddress();
+			int portReceive = receivePacket.getPort();
         	DatagramPacket sendPacket = new DatagramPacket(sendData,
-					sendData.length, IPAddress, portSend);
+					sendData.length, IPAddress, portReceive);
 			serverSocket.send(sendPacket);
         	System.out.println("Data sent.");
         }
-//    	sock.close();
     }
 }
    
