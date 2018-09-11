9b7a7655c2ecfc17f3c5703d2d66cfabc6b4c09c
ehammo <ehammo@cin.ufpe.br>
2018-09-11 20:43:47 -0300

    Middleware benchmark

--- src/analise/benchmark.java
+++ src/analise/benchmark.java
@@ -5,10 +5,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.rmi.NotBoundException;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 
+import middleware.Client;
+import middleware.IEstoque;
 import tcp.ClientTcp;
 import tcp.ServerTcp;
 import udp.ClientUdp;
@@ -17,51 +20,77 @@ public class benchmark {
 	
 	public static void main(String[] args) {
 		
-		ServerTcp server = new ServerTcp();
 		int[] executionAmount = new int[] {50000};
 		ArrayList<Long> results = new ArrayList<>();
-        Instant start;
-        Instant end;
-        System.out.println("TCP test");
-        Path tcpPath = Paths.get("tcp.txt");
-        Path udpPath = Paths.get("udp.txt");
+        long start;
+        long end;
+        System.out.println("Middleware test");
+        Path middlePath = Paths.get("middleware.txt");
         // we assume that both server applications are running on the side.
         try {
-            BufferedWriter tcpWriter = Files.newBufferedWriter(tcpPath);
-			ClientTcp client = new ClientTcp("127.0.0.1", 2005);
-			System.out.println("TCP test");
+            BufferedWriter writer = Files.newBufferedWriter(middlePath);
+            Client client = new Client(IEstoque.host); 
 			for (int i = 0; i < executionAmount.length; i++) {
 				for (int j = 0; j < executionAmount[i]; j++) {
-                    start = Instant.now();
+                    start = System.nanoTime();
                     client.add("new_item");
-                    end = Instant.now();
-                    tcpWriter.write(String.valueOf(Duration.between(start, end).toMillis()) + "\n");
+                    end = System.nanoTime();
+                    writer.write(end-start + "\n");
                     //results.add(Duration.between(start, end).toMillis());
 				}
 
                 //System.out.println(executionAmouznt[i] + " requests took " + Duration.between(start, end).toMillis());
 			}
-			System.out.println("TCP test ended");
-
-			BufferedWriter udpWriter = Files.newBufferedWriter(udpPath);
-            ClientUdp udpClient = new ClientUdp("127.0.0.1",2004);
-            System.out.println("Udp test");
-            for (int i = 0; i < executionAmount.length; i++) {
-                for (int j = 0; j < executionAmount[i]; j++) {
-                    start = Instant.now();
-                    udpClient.add("new_item");
-                    end = Instant.now();
-                    udpWriter.write(String.valueOf(Duration.between(start, end).toMillis()) + "\n");
-                }
-
-                //System.out.println(executionAmount[i] + " requests took " + Duration.between(start, end).toMillis());
-            }
-            System.out.println("UDP test ended");
-
-		} catch (IOException e) {
+			System.out.println("Middleware test ended");
+        } catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+//		ServerTcp server = new ServerTcp();
+//		int[] executionAmount = new int[] {50000};
+//		ArrayList<Long> results = new ArrayList<>();
+//        Instant start;
+//        Instant end;
+//        System.out.println("TCP test");
+//        Path tcpPath = Paths.get("tcp.txt");
+//        Path udpPath = Paths.get("udp.txt");
+//        // we assume that both server applications are running on the side.
+//        try {
+//            BufferedWriter tcpWriter = Files.newBufferedWriter(tcpPath);
+//			ClientTcp client = new ClientTcp("127.0.0.1", 2005);
+//			System.out.println("TCP test");
+//			for (int i = 0; i < executionAmount.length; i++) {
+//				for (int j = 0; j < executionAmount[i]; j++) {
+//                    start = Instant.now();
+//                    client.add("new_item");
+//                    end = Instant.now();
+//                    tcpWriter.write(String.valueOf(Duration.between(start, end).toMillis()) + "\n");
+//                    //results.add(Duration.between(start, end).toMillis());
+//				}
+//
+//                //System.out.println(executionAmouznt[i] + " requests took " + Duration.between(start, end).toMillis());
+//			}
+//			System.out.println("TCP test ended");
+//
+//			BufferedWriter udpWriter = Files.newBufferedWriter(udpPath);
+//            ClientUdp udpClient = new ClientUdp("127.0.0.1",2004);
+//            System.out.println("Udp test");
+//            for (int i = 0; i < executionAmount.length; i++) {
+//                for (int j = 0; j < executionAmount[i]; j++) {
+//                    start = Instant.now();
+//                    udpClient.add("new_item");
+//                    end = Instant.now();
+//                    udpWriter.write(String.valueOf(Duration.between(start, end).toMillis()) + "\n");
+//                }
+//
+//                //System.out.println(executionAmount[i] + " requests took " + Duration.between(start, end).toMillis());
+//            }
+//            System.out.println("UDP test ended");
+//
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
 }
