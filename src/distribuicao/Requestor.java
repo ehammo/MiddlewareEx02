79894fbf979563578f1ba4b9ed0909e7832100f8
Danielle Sales <dso@cin.ufpe.br>
2018-10-09 19:28:27 -0300

    Update Requestor

--- src/distribuicao/Requestor.java
+++ src/distribuicao/Requestor.java
@@ -1,23 +1,20 @@
 package distribuicao;
 
+import distribuicao.message.Message;
 import infraEstrutura.IRequestHandler;
+import infraEstrutura.tcp.TcpClientRequestHandler;
 
 public class Requestor {
-	private IRequestHandler client;
-
-	public Requestor(IRequestHandler client){
-		this.client = client;
-	}
-
-	public Object invoke(Invocation inv){
-		return null;
-	}
-	
-	public IRequestHandler getClient() {
-		return client;
-	}
-
-	public void setClient(IRequestHandler client) {
-		this.client = client;
+	public Termination invoke(Invocation inv) throws Exception{
+		IRequestHandler crh = new TcpClientRequestHandler(inv.getHost(), inv.getPort());
+		
+		Marshaller marshaller = new Marshaller();
+		Termination termination = new Termination(null);
+		Message msgToBeMarshalled = new Message(0, inv.getOperation(), inv.getParameters(), null);
+		
+		crh.send(marshaller.marshall(msgToBeMarshalled));
+		termination.setResult(marshaller.unmarshall(crh.receive()));
+		
+		return termination;
 	}
 }
