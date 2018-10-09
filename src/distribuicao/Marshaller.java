9b4a274fdcd967e5f20c247112cd603faeaf874a
Danielle Sales <dso@cin.ufpe.br>
2018-10-08 22:51:36 -0300

    Add Marshaller implementation

--- src/distribuicao/Marshaller.java
+++ src/distribuicao/Marshaller.java
@@ -1,13 +1,24 @@
 package distribuicao;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
 import distribuicao.message.Message;
 
 public class Marshaller {
-	public byte[] marshall(Message msgToBeMarshalled) {
-		return null;
+	public byte[] marshall(Message msgToBeMarshalled) throws IOException {
+		ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+		ObjectOutputStream objectStream = new ObjectOutputStream(byteStream);
+		objectStream.writeObject(msgToBeMarshalled);
+		return byteStream.toByteArray();
 	}
 
-	public Message unmarshall(byte[] messageToBeUnmarshalled) {
-		return null;
+	public Message unmarshall(byte[] msgToBeUnmarshalled) throws IOException, ClassNotFoundException {
+		ByteArrayInputStream byteStream = new ByteArrayInputStream(msgToBeUnmarshalled);
+		ObjectInputStream objectStream = new ObjectInputStream(byteStream);
+		return (Message) objectStream.readObject();
 	}
 }
