4280fcbaa5a344884c9bcdcd7b22ddee048f11d0
Danielle Sales <dso@cin.ufpe.br>
2018-10-09 21:26:47 -0300

    Message now implements Serializable

--- src/distribuicao/message/Message.java
+++ src/distribuicao/message/Message.java
@@ -1,8 +1,11 @@
 package distribuicao.message;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Message {
+public class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
 	private int requestId;
 	private String operation;
 	private ArrayList<Object> parameters;
